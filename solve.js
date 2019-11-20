62c7f884e7639c3ff48c0e1d5058d0c75baa52e4
Arghyadeep Majumder <40262562+argodeep@users.noreply.github.com>
2019-11-20 13:10:00 +0530

    Update solve.js

--- solve.js
+++ solve.js
@@ -20,4 +20,26 @@ console.log(getFirstRecurringChar(s))
 
 // Q2. STARTS
 
-Comming soon...
+let ar = [1, 2, 5, 6, 3, 4, 4];
+
+function getPairMatchesSum(ar, sum) {
+  let pair = {};
+  let count = 0;
+  ar.map((v, i) => {
+    pair[v] = {
+      value: v,
+      count: pair[v] ? pair[v].count + 1 : 1
+    };
+    let index = sum - pair[v].value;
+    if (pair[index] && pair[v].count === 1) {
+      count ++
+    }
+  })
+  return count
+}
+
+console.log(getPairMatchesSum(ar, 8));
+
+// Q2. ENDS
+
+// Comming soon...
