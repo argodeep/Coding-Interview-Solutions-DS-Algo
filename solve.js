af2094b067b922523990dc78273e40961fc23a68
Arghyadeep Majumder <40262562+argodeep@users.noreply.github.com>
2019-12-23 20:33:22 +0530

    Update solve.js

--- solve.js
+++ solve.js
@@ -57,4 +57,29 @@ function sortedSingleArray(a, b) {
 console.log(sortedSingleArray([1, 11, 4, 6], [5, 8, 9, 10]));
 // Q3. Ends
 
+// Q4 STARTS
+
+let arr = [1, 4, 6, 8, 10, 15, 27];
+let searchValue = 27;
+
+function binarySearch(ar, value, min, max) {
+  let mid = Math.floor((min + max) / 2);
+  if (min > max) { return -1 }
+  if (arr[mid] === value) {
+    return mid
+  }
+  if (arr[mid] < value) {
+    min = mid + 1;
+    return binarySearch(ar, value, min, max)
+  } else {
+    max = mid - 1;
+    return binarySearch(ar, value, min, max)
+  }
+}
+// find value by binary search
+console.log('Got from binary search => ' + binarySearch(arr, searchValue, 0, arr.length - 1));
+console.log('Got from js array findIndex method => ' + arr.findIndex(e => e === searchValue));
+
+// Q4 ENDS
+
 // Comming soon...
